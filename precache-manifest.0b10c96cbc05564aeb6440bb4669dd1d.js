2927eab4c459c3ccc8c6d65119b8521aba1a7ba4
Aalya <aalya.banu@gmail.com>
2020-08-25 17:19:02 +0100

    Updates

---
+++ precache-manifest.0b10c96cbc05564aeb6440bb4669dd1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e358b28c739b62e4c895a0d9fc7cd1",
+    "url": "/WeeklyForecast/index.html"
+  },
+  {
+    "revision": "6c0c2b3a0512457cab3d",
+    "url": "/WeeklyForecast/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "706efd7fc6a389b8b97d",
+    "url": "/WeeklyForecast/static/css/main.f8fae62f.chunk.css"
+  },
+  {
+    "revision": "6c0c2b3a0512457cab3d",
+    "url": "/WeeklyForecast/static/js/2.f4e175fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/WeeklyForecast/static/js/2.f4e175fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706efd7fc6a389b8b97d",
+    "url": "/WeeklyForecast/static/js/main.4c6ee271.chunk.js"
+  },
+  {
+    "revision": "f94d7602c90290e44e2e",
+    "url": "/WeeklyForecast/static/js/runtime-main.c1660c31.js"
+  }
+]);
